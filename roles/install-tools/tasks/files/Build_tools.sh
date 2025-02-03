5afb2bae6bca73bab60a8fcdd84cdd24fa1e6320
trippyp <gavinmerry93@gmail.com>
2025-02-03 15:54:50 +0000

    commit

--- roles/install-tools/tasks/files/Build_tools.sh
+++ roles/install-tools/tasks/files/Build_tools.sh
@@ -10,5 +10,12 @@ bash /opt/linWinPwn/install.sh
 cd /opt/hoaxshell; sudo pip3 install -r requirements.txt
 chmod +x hoaxshell.py
 
+# Build RoadRecon
+cd /opt/ROADRecon
+pip install -e roadlib/
+pip install -e roadrecon/
+cd roadrecon/frontend/
+npm install
+
 # Add all tools in /opt to path
 export PATH=$(find /opt -mindepth 1 -maxdepth 1 -type d -exec sh -c 'find "$1" -maxdepth 1 -type f -executable -print -quit' _ {} \; | tr '\n' ':' | sed 's/:$//'):$PATH
